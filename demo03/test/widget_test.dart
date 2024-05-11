3a79711940f8f8c6ae4974ff835a3f0f0f027b4e
李承斌 <gongjian@163.com>
2024-05-11 14:56:30 +0800

    flutter生物认证示例

--- demo03/test/widget_test.dart
+++ demo03/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:demo2/02.Futuer.dart';
+import 'package:demo03/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
